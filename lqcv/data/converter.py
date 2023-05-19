1bc2a768554bcb74667c8e3d7e0a1fabfbf364ec
Laughing-q <1185102784@qq.com>
2023-05-19 15:39:38 +0800

    update

--- lqcv/data/converter.py
+++ lqcv/data/converter.py
@@ -2,12 +2,14 @@ from abc import ABCMeta, abstractmethod
 from lqcv.bbox import Boxes
 from lqcv.utils.log import LOGGER
 from lqcv.utils.plot import plot_one_box, colors
-from .utils import verify_image_label
+from .utils import IMG_FORMATS
 from multiprocessing.pool import Pool
 from collections import defaultdict
 from tqdm import tqdm
 from tabulate import tabulate
+from pathlib import Path
 import numpy as np
+import os.path as osp
 import os
 import cv2
 
@@ -25,17 +27,17 @@ class BaseConverter(metaclass=ABCMeta):
 
         self.read_labels()
 
-    @abstractmethod
-    def toCOCO(self):
-        pass
-
-    @abstractmethod
-    def toXML(self):
-        pass
-
-    @abstractmethod
-    def toYOLO(self):
-        pass
+    # @abstractmethod
+    # def toCOCO(self):
+    #     pass
+    #
+    # @abstractmethod
+    # def toXML(self):
+    #     pass
+    #
+    # @abstractmethod
+    # def toYOLO(self):
+    #     pass
 
     @abstractmethod
     def read_labels(self):
@@ -52,18 +54,20 @@ class BaseConverter(metaclass=ABCMeta):
         for label in pbar:
             try:
                 filename = label["img_name"]
-                image = cv2.imread(os.path.join(self.img_dir, filename))
+                image = cv2.imread(osp.join(self.img_dir, filename))
                 if image is None:
                     continue
                 cls, bbox = label["cls"], label["bbox"]
                 bbox.convert("xyxy")
                 for i, c in enumerate(cls):
-                    plot_one_box(bbox.data[i], image, color=colors(int(c)), line_thickness=2, label=None)
+                    plot_one_box(
+                        bbox.data[i], image, color=colors(int(c)), line_thickness=2, label=self.class_names[int(c)]
+                    )
             except Exception as e:
                 LOGGER.warning(e)
                 continue
             if save_dir is not None:
-                cv2.imwrite(os.path.join(save_dir, filename), image)
+                cv2.imwrite(osp.join(save_dir, filename), image)
             else:
                 cv2.imshow("p", image)
                 if cv2.waitKey(0) == ord("q"):
@@ -103,10 +107,10 @@ class YOLOConverter(BaseConverter):
                     ├── images
                     ├── labels
         """
-        assert os.path.exists(label_dir), f"The directory '{label_dir}' does not exist."
+        assert osp.exists(label_dir), f"The directory '{label_dir}' does not exist."
         if img_dir is None:
             img_dir = label_dir.replace("labels", "images")
-            assert os.path.exists(
+            assert osp.exists(
                 img_dir
             ), f"The directory '{img_dir}' does not exist, please pass `img_dir` arg."
         super().__init__(label_dir, img_dir, class_names)
@@ -122,7 +126,7 @@ class YOLOConverter(BaseConverter):
         with Pool() as pool:
             pbar = tqdm(
                 pool.imap_unordered(
-                    verify_image_label,
+                    YOLOConverter.verify_image_label,
                     zip(
                         img_names,
                         [self.img_dir] * ni,
@@ -155,11 +159,72 @@ class YOLOConverter(BaseConverter):
         for name, imgCnt in catImg.items():
             self.catImgCnt[name] = len(imgCnt)
 
-    def toCOCO(self):
-        pass
+    @staticmethod
+    def verify_image_label(args):
+        # Verify one image-label pair
+        img_name, imgs_dir, labels_dir, num_classes = args
+        im_file = osp.join(imgs_dir, img_name)
+        lb_file = osp.join(labels_dir, str(Path(img_name).with_suffix(".txt")))
+        nm, nf, ne, nc = 0, 0, 0, 0  # number missing, found, empty, corrupt
+        try:
+            # verify images
+            im = cv2.imread(im_file)
+            shape = im.shape  # h, w, c
+            suffix = Path(im_file).suffix[1:]
+            assert suffix.lower() in IMG_FORMATS, f"invalid image format {suffix}"
 
-    def toXML(self):
-        pass
+            # verify labels
+            if os.path.isfile(lb_file):
+                nf = 1  # label found
+                with open(lb_file, "r") as f:
+                    l = [x.split() for x in f.read().strip().splitlines() if len(x)]
+                    l = np.array(l, dtype=np.float32)
+                if len(l):
+                    assert l.shape[1] == 5, f"labels require 5 columns each: {lb_file}"
+                    assert (l >= 0).all(), f"negative labels: {lb_file}"
+                    assert (
+                        l[:, 1:] <= 1
+                    ).all(), f"non-normalized or out of bounds coordinate labels: {lb_file}"
+                    assert np.unique(l, axis=0).shape[0] == l.shape[0], "duplicate labels"
+                    assert (
+                        l[:, 0] < num_classes
+                    ).all(), f"label cls index out of range, {l[:, 0]}, {lb_file}"
+                else:
+                    ne = 1  # label empty
+                    l = np.zeros((0, 5), dtype=np.float32)
+            else:
+                nm = 1  # label missing
+                l = np.zeros((0, 5), dtype=np.float32)
+            # NOTE: denormlize coordinates
+            l[:, 2::2] *= shape[0]  # h
+            l[:, 1::2] *= shape[1]  # w
+            return img_name, l, shape, nm, nf, ne, nc, ""
+        except Exception as e:
+            nc = 1
+            msg = f"WARNING: Ignoring corrupted image and/or label {im_file}: {e}"
+            return [None, None, None, nm, nf, ne, nc, msg]
 
-    def toYOLO(self):
-        pass
+
+class XMLConverter(BaseConverter):
+    def __init__(self, label_dir, img_dir=None, class_names=None) -> None:
+        """XMLConverter.
+
+        Args:
+            label_dir (str): The directory of .xml labels.
+            class_names (str): Class names.
+            img_dir (str | optional): Image directory,
+                if it's None then assume the structure is like the following example:
+                    root/
+                    ├── images
+                    ├── xmls
+        """
+        assert osp.exists(label_dir), f"The directory '{label_dir}' does not exist."
+        if img_dir is None:
+            img_dir = label_dir.replace("labels", "images")
+            assert osp.exists(
+                img_dir
+            ), f"The directory '{img_dir}' does not exist, please pass `img_dir` arg."
+        super().__init__(label_dir, img_dir, class_names)
+
+    def read_labels(self):
+        LOGGER.info(f"Read xml labels from {self.label_dir}...")
