b34be66a44a479c7eedf061ebde05ef25a2f3339
Laughing-q <1185102784@qq.com>
2023-05-29 19:32:32 +0800

    udpate similarity

--- lqcv/tools/image.py
+++ lqcv/tools/image.py
@@ -7,7 +7,7 @@ import cv2
 import shutil
 
 
-def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, end=-1, stype="phash", save=True):
+def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, end=-1, stype="phash", save=True, name=''):
     """Compute the similarity between images and remove these images with high similarity.
 
     Args:
@@ -20,16 +20,17 @@ def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, e
         stype (str): The calculation type of similarity, could be `phash` and `img`.
         save (bool): There are two modes, save mode and remove mode. Usually the workflow is save(mode) first 
             then remove(mode).
+        name (str): The save name for HashValues and HashNames.
     """
     assert stype in ["phash", "img"]
     compute = pHash if stype == "phash" else img_hash
     compare = cmpHash_np if stype == "phash" else cmpImgHash_np
 
     if save:
-        if osp.exists("hashValue.txt"):
-            os.remove("hashValue.txt")
-        if osp.exists("hashName.txt"):
-            os.remove("hashName.txt")
+        if osp.exists(f"{name}Value.txt"):
+            os.remove(f"{name}Value.txt")
+        if osp.exists(f"{name}Name.txt"):
+            os.remove(f"{name}Name.txt")
 
         imgs_lists = sorted(os.listdir(img_dir))
         pbar = tqdm(imgs_lists, total=len(imgs_lists))
@@ -39,21 +40,22 @@ def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, e
             if img is None:
                 os.remove(osp.join(img_dir, p))   # remove broken images.
                 continue
-            with open("hashValue.txt", "a") as fv:
+            with open(f"{name}Value.txt", "a") as fv:
                 fv.write(str(compute(img)).replace("[", "").replace("]", "").replace(",", "") + "\n")
-            with open("hashName.txt", "a") as fn:
+            with open(f"{name}Name.txt", "a") as fn:
                 fn.write(p + "\n")
     else:
         os.makedirs(remove_dir, exist_ok=True)
 
-        with open("hashName.txt", "r") as f:
+        with open(f"{name}Name.txt", "r") as f:
             names = [i.strip() for i in f.readlines()][start:end]
-        values = np.loadtxt("hashValue.txt", dtype=np.uint8)[start:end]
-        print(values.shape)
+        values = np.loadtxt(f"{name}Value.txt", dtype=np.uint8)[start:end]
         removed_idx = []
         counter = 0
 
-        for i, v in tqdm(enumerate(values), total=len(values)):
+        pbar = tqdm(enumerate(values), total=len(values))
+        pbar.desc = f"{name}"
+        for i, v in pbar:
             d = compare(v, values)  # distance
             if d.ndim == 2:
                 d = d.squeeze(-1)
@@ -65,7 +67,6 @@ def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, e
                 continue
             # idx_dir = osp.join(target_dir, f"{i + start}")
             idx_dir = osp.join(remove_dir, f"{names[i]}")
-            os.makedirs(idx_dir, exist_ok=True)
             for idx in indexes:
                 if idx in removed_idx:
                     continue
@@ -73,8 +74,9 @@ def similarity(img_dir, remove_dir, threshold=0.95, count_only=False, start=0, e
                 counter += 1
                 if count_only:
                     continue
+                os.makedirs(idx_dir, exist_ok=True)
                 shutil.move(osp.join(img_dir, names[idx]), idx_dir)
-        print("keep counter:", len(values) - counter)
+        print(f"keep counter:{len(values) - counter}/{len(values)}", )
 
 
 def generate_fog(img):
