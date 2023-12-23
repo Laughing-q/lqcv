ff1e3886f3d042b8cb68ce56925515b2b9f36f5f
Laughing-q <1182102784@qq.com>
2023-12-23 22:06:22 +0800

    update requirements.txt && setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
-import re
+from pathlib import Path
 import setuptools
-# import torch
+import re
 
-# torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
-# assert torch_ver >= [1, 7], "Requires PyTorch >= 1.7"
+
+def parse_requirements(file_path: Path):
+    """
+    Parse a requirements.txt file, ignoring lines that start with '#' and any text after '#'.
+
+    Args:
+        file_path (str | Path): Path to the requirements.txt file.
+
+    Returns:
+        (List[str]): List of parsed requirements.
+    """
+
+    requirements = []
+    for line in Path(file_path).read_text().splitlines():
+        line = line.strip()
+        if line and not line.startswith('#'):
+            requirements.append(line.split('#')[0].strip())  # ignore inline comments
+
+    return requirements
 
 with open("lqcv/__init__.py", "r") as f:
     version = re.search(
@@ -25,4 +42,5 @@ setuptools.setup(
     classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
     # cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
     packages=setuptools.find_packages(),
+    install_requires=parse_requirements(Path(__file__).resolve().parent / 'requirements.txt'),
 )
