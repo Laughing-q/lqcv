c898b86cbe3f15120e106e85388c2c499e67cb66
Laughing-q <1185102784@qq.com>
2024-03-16 14:56:05 +0800

    attempt to fix resume

--- lqcv/ultralytics/trainer.py
+++ lqcv/ultralytics/trainer.py
@@ -1,6 +1,5 @@
 from ultralytics.models.yolo import detect, pose, segment
-from ultralytics.utils import RANK, yaml_save
-from ultralytics.utils import colorstr
+from ultralytics.utils import RANK, yaml_save, colorstr, IterableSimpleNamespace
 from copy import copy
 from .dataset import LQDataset
 
@@ -9,15 +8,17 @@ class DetectionTrainer(detect.DetectionTrainer):
     def __init__(self, overrides=None, _callbacks=None, extra_args={}):
         super().__init__(overrides=overrides, _callbacks=_callbacks)
         # pass all extra_args to self.args
-        self.val_args = copy(self.args)
+        # NOTE: For resume, exclude extra keys.
+        self.val_args = IterableSimpleNamespace(**{k: v for k, v in copy(self.args).items() if k not in extra_args})
         for k, v in extra_args.items():
             self.args.__setattr__(k, v)
         # save the args again with extra_args
         if RANK in (-1, 0):
-            yaml_save(self.save_dir / 'args.yaml', vars(self.args))  # save run args
+            yaml_save(self.save_dir / "args.yaml", vars(self.args))  # save run args
 
-    def build_dataset(self, img_path, mode='train', batch=None):
+    def build_dataset(self, img_path, mode="train", batch=None):
         from ultralytics.utils.torch_utils import de_parallel
+
         gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
         cfg = self.args
         return LQDataset(
@@ -45,11 +46,12 @@ class DetectionTrainer(detect.DetectionTrainer):
             self.test_loader, save_dir=self.save_dir, args=copy(self.val_args), _callbacks=self.callbacks
         )
 
+
 class PoseTrainer(DetectionTrainer, pose.PoseTrainer):
     def __init__(self, overrides=None, _callbacks=None, extra_args={}):
         if overrides is None:
             overrides = {}
-        overrides['task'] = 'pose'
+        overrides["task"] = "pose"
         DetectionTrainer.__init__(self, overrides=overrides, _callbacks=_callbacks, extra_args=extra_args)
 
 
@@ -57,5 +59,5 @@ class SegmentationTrainer(DetectionTrainer, segment.SegmentationTrainer):
     def __init__(self, overrides=None, _callbacks=None, extra_args={}):
         if overrides is None:
             overrides = {}
-        overrides['task'] = 'segment'
+        overrides["task"] = "segment"
         DetectionTrainer.__init__(self, overrides=overrides, _callbacks=_callbacks, extra_args=extra_args)
