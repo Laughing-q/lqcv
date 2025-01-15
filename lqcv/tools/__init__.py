03ff462db9e12d0914c29a895b66cf52864b4342
Laughing-q <1185102784@qq.com>
2025-01-15 16:31:22 +0800

    add similarity_yolo

--- lqcv/tools/__init__.py
+++ lqcv/tools/__init__.py
@@ -1,4 +1,4 @@
 from .video import videos2images
-from .image import generate_fog, similarity
+from .image import generate_fog, similarity, similarity_yolo
 from .file import remove_extra_files, find_extra_names, split_images, split_images_labels, get_files
 from .export import export_yolov8
