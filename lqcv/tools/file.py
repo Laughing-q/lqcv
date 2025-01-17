685bba0307298e6128260df983d1048020e13305
Laughing-q <1185102784@qq.com>
2025-01-17 22:49:57 +0800

    Update file.py

--- lqcv/tools/file.py
+++ lqcv/tools/file.py
@@ -31,7 +31,7 @@ def find_extra_names(more_dir, less_dir, reverse=False, pure=False):
     return return_names
 
 
-def remove_extra_files(more_dir, less_dir, target_dir=None, reverse=False):
+def remove_extra_files(more_dir, less_dir, target_dir=None, reverse=False, copy=False):
     """Remove the extra files that in `more_dir` but not included in `less_dir`.
 
     Args:
@@ -41,13 +41,17 @@ def remove_extra_files(more_dir, less_dir, target_dir=None, reverse=False):
             this function would directly remove extra files if `target_dir` is None(by default).
         reverse (bool): This function removes/moves these extra files by default(reverse=False),
             and it removes/moves the common files if reverse=True.
+        copy (bool): If copy=True, then copy the extra files to `target_dir`, otherwise move them.
     """
     names = find_extra_names(more_dir=more_dir, less_dir=less_dir, reverse=reverse)
     if target_dir:
         os.makedirs(target_dir, exist_ok=True)
     for name in names:
         file = os.path.join(more_dir, name)
-        shutil.move(file, target_dir) if target_dir else os.remove(file)
+        if target_dir:
+            shutil.copy(file, target_dir) if copy else shutil.move(file, target_dir)
+        else:
+            os.remove(file)
 
 
 def split_images(data_dir, ratio=0.8):
