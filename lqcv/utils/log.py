6523e521ffed42be69dc3bc0babd474e7e8cd0e5
Laughing-q <1185102784@qq.com>
2026-02-10 11:05:50 +0800

    Update utils/log.py

--- lqcv/utils/log.py
+++ lqcv/utils/log.py
@@ -1,24 +1,19 @@
 import logging.config
 
+
 def set_logging(name="lqcv", verbose=True, format="%(message)s"):
     level = logging.INFO if verbose else logging.ERROR
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            name: {
-                'format': f"{format}"}},
-        'handlers': {
-            name: {
-                'class': 'logging.StreamHandler',
-                'formatter': name,
-                'level': level}},
-        'loggers': {
-            name: {
-                'level': level,
-                'handlers': [name],
-                'propagate': False}}})
-    return 
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {name: {"format": f"{format}"}},
+            "handlers": {name: {"class": "logging.StreamHandler", "formatter": name, "level": level}},
+            "loggers": {name: {"level": level, "handlers": [name], "propagate": False}},
+        }
+    )
+    return
+
 
 set_logging()
-LOGGER = logging.getLogger('lqcv')
+LOGGER = logging.getLogger("lqcv")
